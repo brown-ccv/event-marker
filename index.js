f86521d58174b46b56972f86a9f92cfd937ac592
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-12-10 10:18:39 -0500

    feat: dispatch based on number of arguments

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 var SerialPort = require('serialport');
 
 
-exports.isPort = async (comName) => {
+const deviceByComName = (portList, comName) => {
+  const device = portList.filter((device) => {
+    return (device.comName === comName.toUpperCase() ||
+            device.comName === comName);
+  })
+
+  return device
+}
+
+const deviceByProductId = (portList, vendorId, productId) => {
+  const device = portList.filter((device) => {
+    return ((device.vendorId === vendorId.toUpperCase() ||
+            device.vendorId === vendorId) && device.productId === productId);
+  })
+
+  return device
+}
+
+// dispatcher based on one argument to ids or two
+const getDevice = (portList, comVendorName, productId) => {
+  if (productId === undefined) {
+    return deviceByComName(portList, comVendorName)
+  } else {
+    return deviceByProductId(portList, comVendorName, productId)
+  }
+}
+
+const isPort = async (comVendorName, productId) => {
   let portList
   try {
     portList = await SerialPort.list()
   } catch {
     return false
   }
-  const device = portList.filter((device) => {
-    return (device.comName === comName.toUpperCase() ||
-            device.comName === comName);
-  })
+  const device = getDevice(portList, comVendorName, productId)
   if (device.length === 1) {
     return true
   }
@@ -20,30 +44,24 @@ exports.isPort = async (comName) => {
   }
 }
 
-exports.getPort = async (comName) => {
-  let portList
-  try {
-    portList = await SerialPort.list()
-  } catch {
-    return false
-  }
-  const device = portList.filter((device) => {
-    return (device.comName === comName.toUpperCase() ||
-            device.comName === comName);
-  })
+const getPort = async (comVendorName, productId) => {
+  const portList = await SerialPort.list()
+  const device = getDevice(portList, comVendorName, productId)
   try {
     const path = device[0].comName;
     const port = new SerialPort(path)
     return port
   } catch {
-    return false
+    return {}
   }
 }
 
-exports.sendToPort = async (port, event_code) => {
-  try {
-    await port.write(Buffer.from([event_code]))
-  } catch (error) {
-    throw Error(error)
-  }
+const sendToPort = async (port, event_code) => {
+  port.then(p => p.write(Buffer.from([event_code])))
+}
+
+module.exports = {
+  isPort,
+  getPort,
+  sendToPort
 }
--- test/test.js
+++ test/test.js
@@ -2,22 +2,44 @@ var should = require('chai').should();
 var eventmarker = require('../index.js');
 
 const comName = 'com7';
+const vendorId = 'teensyduino'
+const productId = '1203'
 
 describe("Ports", () => {
 
-  it("If no port, is port is empty", (done) => {
-    eventmarker.getPort(comName)
-      .then((res) => {
-        res.should.be.empty
-        done()
-      })
-  });
+  describe("with com name only", () => {
+    it("If no port, is port is empty", (done) => {
+      eventmarker.getPort(comName)
+        .then((res) => {
+          res.should.be.empty
+          done()
+        })
+    });
 
-  it("If no port, has no port", (done) => {
-    eventmarker.isPort(comName)
-      .then((res) => {
-        res.should.be.false
-        done()
-      })
+    it("If no port, has no port", (done) => {
+      eventmarker.isPort(comName)
+        .then((res) => {
+          res.should.be.false
+          done()
+        })
+    })
+  })
+
+  describe("with vendor and product ids", () => {
+    it("If no port, is port is empty", (done) => {
+      eventmarker.getPort(vendorId, productId)
+        .then((res) => {
+          res.should.be.empty
+          done()
+        })
+    });
+
+    it("If no port, has no port", (done) => {
+      eventmarker.isPort(vendorId, productId)
+        .then((res) => {
+          res.should.be.false
+          done()
+        })
+    })
   })
 });
